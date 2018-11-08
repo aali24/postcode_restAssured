389a2dcee6d735a5e85b2e21a5df34dce59b1a5e
abdi ali <aali@spartaglobal.com>
2018-11-08 09:46:04 +0000

    initial commit

---
+++ src/test/java/postcode_restAssured/AppTest.java
@@ -0,0 +1,20 @@
+package postcode_restAssured;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
